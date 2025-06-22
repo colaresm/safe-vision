1489b7958565ad1d275a87a2bfff8324c9df3c4d
colaresmarcelo2018 <colaresmarcelo2018@gmail.com>
2025-06-22 11:28:56 -0300

    Insert detecction of ppe on endpoint stream

--- services/ppi_detecction.py
+++ services/ppi_detecction.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import matplotlib.pyplot as plt
 import cv2
-
+ 
 def iou(box1, box2):
     xA = max(box1[0], box2[0])
     yA = max(box1[1], box2[1])
@@ -17,48 +17,37 @@ def iou(box1, box2):
 
     return interArea / float(box1Area + box2Area - interArea)
 
-detect_ppe_model = YOLO('../models/ppi_detecction.pt')
-base_model = YOLO('../models/yolov8n.pt')
-
-source_path = 'kx.37.12.png'
-
-results_ppe = detect_ppe_model(source_path)
-results_base = base_model(source_path)
-
-ppe_boxes = []
-for r_ppe in results_ppe:
-    for box in r_ppe.boxes:
-        x1, y1, x2, y2 = map(int, box.xyxy[0])
-        ppe_boxes.append((x1, y1, x2, y2))
-
-image = cv2.imread(source_path)
-
-for r in results_base:
-    for box in r.boxes:
-        class_id = int(box.cls[0])
-        class_name = base_model.names[class_id]
-
-        if class_name == 'person':
+def detect_ppe(frame, results_base, results_ppe, base_model):
+    ppe_boxes = []
+    for r_ppe in results_ppe:
+        for box in r_ppe.boxes:
             x1, y1, x2, y2 = map(int, box.xyxy[0])
-            person_box = (x1, y1, x2, y2)
+            ppe_boxes.append((x1, y1, x2, y2))
 
-            has_ppe = False
-            for eb in ppe_boxes:
-                if iou(person_box, eb) > 0.1:
-                    has_ppe = True
-                    break
+    for r in results_base:
+        for box in r.boxes:
+            class_id = int(box.cls[0])
+            class_name = base_model.names[class_id]
 
-            if  has_ppe:
-                label = 'With PPE'
-                color = (0, 255, 0)   
-            else:
-                label = 'Without PPE'
-                color = (0, 0, 255)   
+            if class_name == 'person':
+                x1, y1, x2, y2 = map(int, box.xyxy[0])
+                person_box = (x1, y1, x2, y2)
 
-            cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
-            cv2.putText(image, label, (x1 + 20, y2),
-                        cv2.FONT_HERSHEY_SIMPLEX, 3, color, 3)
+                has_ppe = False
+                for eb in ppe_boxes:
+                    if iou(person_box, eb) > 0.1:
+                        has_ppe = True
+                        break
 
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-plt.imshow(image_rgb)
-plt.show()
+                if has_ppe:
+                    label = 'With PPE'
+                    color = (0, 255, 0)
+                else:
+                    label = 'Without PPE'
+                    color = (0, 0, 255)
+
+                cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                cv2.putText(frame, label, (x1 + 20, y2),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+
+    return frame
--- services/stream.py
+++ services/stream.py
@@ -1,9 +1,13 @@
 from ultralytics import YOLO
 import cv2
 import time
+from services.ppi_detecction import detect_ppe
 
 model = YOLO('models/yolov8n.pt')
 video = cv2.VideoCapture("static/video.mp4")
+
+base_model = YOLO('models/yolov8n.pt')
+ppe_model = YOLO('models/ppi_detecction.pt')   
 
 def generate_frames():
     global video
@@ -12,8 +16,12 @@ def generate_frames():
         if not success:
             video.set(cv2.CAP_PROP_POS_FRAMES, 0)
             continue
-        results = model(frame)[0]
-        annotated_frame = results.plot()
+
+        results_base = base_model(frame)
+        results_ppe = ppe_model(frame)
+
+        annotated_frame = detect_ppe(frame, results_base, results_ppe, base_model)
+
         ret, buffer = cv2.imencode('.jpg', annotated_frame)
         if not ret:
             continue
